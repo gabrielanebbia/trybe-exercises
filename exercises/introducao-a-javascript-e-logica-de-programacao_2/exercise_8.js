d52f8491c98e095b086055fda5d69ed5df9bee55
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-03-29 02:50:45 -0300

    Exercicio 8 - Cria com o for um array de 1 ate 25

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise_8.js
@@ -0,0 +1,9 @@
+//Exercicio 8 - Utilizando o for, crie um array que vá 1  até o 25 e imprima o resultado.
+
+let array = [];
+
+for (let contador = 1; contador <= 25; contador += 1) {
+    array.push(contador);
+}
+
+console.log(array);
