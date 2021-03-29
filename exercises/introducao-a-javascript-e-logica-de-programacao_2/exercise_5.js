a90a1f3badf7daca6191d9013bd27ab345971ae9
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-03-29 02:32:00 -0300

    Exercicio 5 - Corrige variavel index

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise_5.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise_5.js
@@ -4,7 +4,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let maiorNumber = 0;
 
-for (var index = 0; index < numbers.length; index += 1) {
+for (let index = 0; index < numbers.length; index += 1) {
    if (numbers[index] > maiorNumber) {
       maiorNumber = numbers[index];
    }
