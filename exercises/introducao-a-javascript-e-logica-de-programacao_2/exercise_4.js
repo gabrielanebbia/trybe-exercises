d8972b2b8ed42a7e5b33595745c5c5ddf0a5e90b
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-03-29 02:01:22 -0300

    Exercicio 4 - Imprime mensagem em relacao ao valor da media aritmetica calculada do array

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise_4.js
@@ -0,0 +1,20 @@
+//Exercicio 4 - Imprima a mensagem em relação a média aritmética calculada.
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let soma = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    soma += numbers[index];  
+    console.log(soma);
+}
+
+let mediaAritmetica = soma / numbers.length;
+
+console.log(mediaAritmetica);
+
+if(mediaAritmetica > 20) {
+    console.log('Valor maior que 20');
+} else {
+    console.log('Valor menor ou igual a 20');
+}
