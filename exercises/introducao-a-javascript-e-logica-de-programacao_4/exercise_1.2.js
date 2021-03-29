af5aeb5e85047b2b7a825feaad3e223e6d32c2cf
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-03-29 19:27:59 -0300

    Exercicio 1.2 - Insere no objeto uma propriedade e imprime-a

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/exercise_1.2.js
@@ -0,0 +1,11 @@
+//Exercicio 1.2 - Insira no objeto uma nova propriedade e imprima-o.
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim',
+  };
+
+  console.log(info.recorrente);
+  
