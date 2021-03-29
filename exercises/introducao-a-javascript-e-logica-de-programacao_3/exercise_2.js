27d7dd32d8be4e80fdd777ac56924f309c5b656e
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-03-29 13:13:08 -0300

    Exercicio 2 - Imprimindo um triangulo retangulo de asteristicos

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercise_2.js
@@ -0,0 +1,22 @@
+//Exercicio 2 - Faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um triângulo retângulo com 5 asteriscos de base.
+
+/* 
+n = 5
+
+*
+**
+***
+****
+*****
+
+ */
+
+
+let quadrado = [];
+let numero = 5;
+let asteristico = "*";
+
+for (let index = 1; index <= numero; index += 1) {
+   quadrado.push(asteristico);
+   console.log(quadrado);
+}
