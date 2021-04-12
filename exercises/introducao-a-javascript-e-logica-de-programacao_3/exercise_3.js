db1a768c64f072882ddb556c6b07d6347cbcff2e
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-04-11 23:36:23 -0300

    Exercicio 3 - imprime um triangulo de base invertida

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercise_3.js
@@ -0,0 +1,32 @@
+//Exercicio 3 - Faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um triângulo retângulo com 5 asteriscos de base invertido.
+
+/* 
+n = 5
+
+    *
+   **
+  ***
+ ****
+***** 
+
+*/
+
+let numero = 5;
+let asteristico = '*';
+let espaco = '';
+let posicao = numero;
+
+for (let index = 0; index < numero; index += 1) {
+  for (let index2 = 0; index2 <= numero; index2 += 1) {
+    if (index2 < posicao) {
+      espaco = espaco + ' ';
+    } else {
+      espaco = espaco + asteristico;
+    }
+  }
+  console.log(espaco);
+  espaco = '';
+  posicao -= 1;
+}
+
+
