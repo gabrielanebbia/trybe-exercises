f7aa8838abff7dd44445e0734a8392ce0035b606
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-04-20 02:55:44 -0300

    Cria outro fieldset para dados do ultimo emprego

--- exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
+++ exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -1,3 +1,4 @@
+// Cria através de uma estrutura de repetição os <option>
 let estados = ['Selecione','Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal',  'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
 
 let secaoEstado = document.querySelector('#userEstado');
@@ -7,3 +8,24 @@ for (let index = 0; index < estados.length; index += 1) {
   novoEstado.innerText = estados[index];
   secaoEstado.appendChild(novoEstado);
 }
+
+//Cria alertas de erro para datas inválidas
+function testDataInicial() {
+let dataInput = document.getElementById('userDataInicio').value;
+
+  for (let index = 0; index < dataInput.length; index += 1) {
+    if (dataInput[index] === '-') {
+    alert('Data inválida');
+    }
+    if ((dataInput[0] + dataInput[1]) > 31) {
+      alert('Data inválida');
+    }
+    if ((dataInput[2] + dataInput[3]) > 12) {
+      alert('Mês inválido');
+    }
+  }
+}
+
+testDataInicial();
+
+dataInput.addEventListener('keyup', testDataInicial);
