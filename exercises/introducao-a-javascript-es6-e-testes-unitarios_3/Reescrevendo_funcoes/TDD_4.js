70128f2d2d988ca0175144d2115a9a712015ca39
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-05-03 02:28:46 -0300

    Parte 3 - exercicio 4

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_3/Reescrevendo_funcoes/TDD_4.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_3/Reescrevendo_funcoes/TDD_4.js
@@ -1,7 +1,21 @@
-function secondThirdSmallest(array) {
-  let results = []
+const assert = require('assert');
+
+/* function secondThirdSmallest(array) {
+  let results = [];
   array.sort(function (x, y) {
       return x + y;
+  });
+  results = [array[1], array[2]];
+  return results;
+};
+ */
+
+function secondThirdSmallest(array) {
+  let results = [];
+  array.sort(function compare(x, y) {
+      if(x > y) return 1;
+      if(x < y) return -1;
+      return 0;
   });
   results = [array[1], array[2]];
   return results;
@@ -9,3 +23,12 @@ function secondThirdSmallest(array) {
 
 const parameter = [4, 10, 32, 9, 21, 90, 5, 11, 8, 6];
 const result = [5, 6];
+
+/* function secondThirdSmallest(array) { //Forma simplificada de escrever a mesma função acima
+  let results = [];
+  array.sort((x, y) => x - y);
+  results = [array[1], array[2]];
+  return results;
+} */
+
+assert.deepStrictEqual(secondThirdSmallest(parameter), result);
