a0392128044f0da440572ba327b802ce14e047aa
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-05-03 02:11:52 -0300

    Recoloca enunciado do exercicio

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_3/Reescrevendo_funcoes/TDD_3.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_3/Reescrevendo_funcoes/TDD_3.js
@@ -1,5 +1,15 @@
 const assert = require('assert');
 
+/* const greaterThanTen = (array) => {
+  let results = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] > 10) {
+      results += array[index];
+    }
+  }
+  return results;
+}; */
+
 const greaterThanTen = (array) => {
   let results = [];
   for (let index = 0; index < array.length; index += 1) {
