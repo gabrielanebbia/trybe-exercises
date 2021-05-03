acee53778e7136ed6cdacb45c900cbe866f4c85f
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-05-03 02:05:01 -0300

    Parte 3 - exercicio 2

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_3/Reescrevendo_funcoes/TDD_2.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_3/Reescrevendo_funcoes/TDD_2.js
@@ -1,4 +1,6 @@
-const removeVowels = (word) => {
+const assert = require('assert');
+
+/* const removeVowels = (word) => {
   const characters = word.split('');
   const results = [];
 
@@ -16,8 +18,31 @@ const removeVowels = (word) => {
     }
   }
   return results;
-};
+}; */
 
+const removeVowels = (word) => {
+  const characters = word.split('');
+  const results = [];
+  let position = 0;
+
+  for (let index = 0; index < characters.length; index += 1) {
+    if (
+      characters[index] === 'a' ||
+      characters[index] === 'o' ||
+      characters[index] === 'i' ||
+      characters[index] === 'e' ||
+      characters[index] === 'u'
+    ) {
+      position += 1;
+      results.push(position);
+    } else {
+      results.push(characters[index]);
+    }
+  }
+  return results.join('');
+};
 
 const parameter = 'Dayane';
 const result = 'D1y2n3';
+
+assert.deepStrictEqual(removeVowels(parameter), result);
