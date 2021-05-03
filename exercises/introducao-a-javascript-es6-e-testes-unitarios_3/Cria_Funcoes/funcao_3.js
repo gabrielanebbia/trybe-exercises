eed800b9d160f79f8c2490fc320fb411d3e418d5
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-05-02 23:34:16 -0300

    Parte 2 - exercicio 3

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_3/Cria_Funcoes/funcao_3.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_3/Cria_Funcoes/funcao_3.js
@@ -1,5 +1,12 @@
 const assert = require('assert');
 // escreva a função sumAllNumbers aqui
+function sumAllNumbers(number) {
+  let sum = 0;
+  for(let index = 0; index < number.length; index += 1) {
+    sum += number[index];
+  }
+  return sum;
+};
 
 const numbers = [9, 23, 10, 3, 8];
 const expected = 53;
