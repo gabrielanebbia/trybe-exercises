69650483116e4a6b6822348f1d9652d591b1f5f1
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-05-07 18:01:16 -0300

    Exercicio 1 - Cria funcao de primeira classe como parametro da HOF

--- exercises/exercicio_1.js
+++ exercises/exercicio_1.js
@@ -1,8 +1,27 @@
-const newEmployees = () => {
+/* const newEmployees = () => {
   const employees = {
     id1: // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro
     id2: // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro
     id3: // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro
   }
   return employees;
+}; */
+
+const pessoaContratada = (nomeCompleto) => {
+  const pessoa = {
+    nomeCompleto: `${nomeCompleto}`,
+    email: `${nomeCompleto.toLowerCase().split(' ').join('_')}@trybe.com`
+  };
+  return pessoa;
 };
+
+const newEmployees = (pessoaContratada) => {
+  const employees = {
+    id1: pessoaContratada('Pedro Guerra'), 
+    id2: pessoaContratada('Luiza Drumond'),
+    id3: pessoaContratada('Carla Paiva'),
+  };
+  return employees;
+};
+
+console.log(newEmployees(pessoaContratada));
--- exercises/higher-order-functions-do-javascript-es6_1/README.md
+++ exercises/higher-order-functions-do-javascript-es6_1/README.md
@@ -3,4 +3,7 @@
 
 #### Exercício 1
 
+- [x] Crie uma função de primeira classe que retorna um objeto { nomeCompleto, email } de uma nova pessoa contratada.
+- [x] Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respctivo id. A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com .
+
 #### Exercício 2
