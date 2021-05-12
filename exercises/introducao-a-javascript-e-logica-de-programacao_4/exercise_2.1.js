35cf97860455857b4592c25ecd3a8aafec3fa642
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-05-11 21:36:53 -0300

    Exercicio 2.1 - Palindrome

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/exercise_2.1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/exercise_2.1.js
@@ -1,6 +1,24 @@
-function verificaPalindrome() {
-
+function verificaPalindrome(word) {
+  let letters = word.split('');
+  let palindrome = true;
+  for (let index in letters) {
+    if (letters[index] != word[(word.length - 1) - index]) {
+      palindrome = false;
+    }
+  }
+  return palindrome;
 };
 
-verificaPalindrome('arara'); // true
-verificaPalindrome('desenvolvimento'); // false
+// Outra forma de resolver (Gabarito):
+
+function verificaPalindrome(string) {
+  let reverse = string.split('').reverse().join('');
+  if (reverse === string) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(verificaPalindrome('arara')); // true
+console.log(verificaPalindrome('desenvolvimento')); // false
