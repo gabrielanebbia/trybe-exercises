36cad490e87b2e6bc281a323bd502767fc633c6d
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-05-11 21:48:19 -0300

    Exercicio 2.2 - Imprime o indice do maior numero

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/exercise_2.2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/exercise_2.2.js
@@ -1,5 +1,12 @@
-function highestValueIndex () {
+function highestValueIndex (integerArray) {
+  let higherNumber = 0;
+  for (let index in integerArray) {
+    if (integerArray[higherNumber] < integerArray[index]){
+      higherNumber = index;
+    }
+  }
+  return higherNumber;
 
 };
 
-highestValueIndex([2, 3, 6, 7, 10, 1]); //4
+console.log(highestValueIndex([2, 3, 6, 7, 10, 1])); //4
