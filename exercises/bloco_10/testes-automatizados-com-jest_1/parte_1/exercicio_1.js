bcbb519bd1d99c281c7674dd32baa178e3442149
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-05-19 18:58:11 -0300

    Exercicio 1 - funcao sum e teste Jest

--- exercises/bloco_10/testes-automatizados-com-jest_1/parte_1/exercicio_1.js
+++ exercises/bloco_10/testes-automatizados-com-jest_1/parte_1/exercicio_1.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+// const assert = require('assert');
 
 function sum(a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
@@ -10,9 +10,10 @@ function sum(a, b) {
 
 // implemente seus testes aqui
 // Testes unitários em JavaScript:
-assert.strictEqual(sum(4, 5), 9);
+/* assert.strictEqual(sum(4, 5), 9);
 assert.strictEqual(sum(0, 0), 0);
 assert.throws(() => {sum(4, '5');});
-assert.throws(() => {sum(4, '5')}, /^Error: parameters must be numbers$/);
+assert.throws(() => {sum(4, '5')}, /^Error: parameters must be numbers$/); */
 
-// Testes automatizados com Jest:
+// Testes automatizados com Jest
+module.exports = sum;
---
+++ exercises/bloco_10/testes-automatizados-com-jest_1/parte_1/exercicio_1.test.js
@@ -0,0 +1,5 @@
+const sum = require('./exercicio_1');
+
+test('sums two values', () => {
+  expect(sum(4, 5)).toEqual(9);
+ });
