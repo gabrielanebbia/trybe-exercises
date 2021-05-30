d27ea5ec2ed158c0f03da45d369311caa270be6c
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-05-29 23:33:34 -0300

    Cria estilo da pagina

--- exercises/modulo_2/bloco_11/introducao-a-react_2/my-pokedex/src/Pokemon.jsx
+++ exercises/modulo_2/bloco_11/introducao-a-react_2/my-pokedex/src/Pokemon.jsx
@@ -4,10 +4,14 @@ class Pokemon extends React.Component {
   render() {
     return (
       <div className='pokemon'>
-        <p>{ this.props.name }</p>
-        <p>{ this.props.type }</p>
-        <p>Average weight { this.props.weight } {this.props.unit }</p>
-        <img src={ this.props.image }/>
+        <div className='pokemon-text'>
+          <p>{ this.props.name }</p>
+          <p>{ this.props.type }</p>
+          <p>Average weight { this.props.weight } {this.props.unit }</p>
+        </div>
+        <div className='pokemon-img'>
+          <img src={ this.props.image }/>
+        </div>
       </div>
     );
   }
