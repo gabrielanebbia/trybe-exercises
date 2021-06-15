ba026c31f31d50c9df5c144129033303159c8752
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-06-14 22:33:19 -0300

    Revert "Bloco 4 - (Dia 2) Introdução à Javascript e Lógica de Programação - Array e loop For"

--- exercises/modulo_1/bloco_4/introducao-a-javascript-e-logica-de-programacao_2/exercise_8.js
+++
@@ -1,7 +0,0 @@
-let array = [];
-
-for (let contador = 1; contador <= 25; contador += 1) {
-    array.push(contador);
-}
-
-console.log(array);
--- exercises/modulo_1/bloco_4/introducao-a-javascript-e-logica-de-programacao_2/exercise_9.js
+++
@@ -1,7 +0,0 @@
-let array = [];
-
-for (let contador = 1; contador <= 25; contador += 1) {
-    array.push(contador / 2);
-}
-
-console.log(array);
