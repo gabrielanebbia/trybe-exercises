601de101b8354043d960b1f35f8fcd29d3ec49b1
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-08-23 17:37:52 -0300

    Exercicio bonus 5

--- modulo_3.Back-end/bloco_20/introducao-a-sql.3/20.3_exercise_bonus.sql
+++ modulo_3.Back-end/bloco_20/introducao-a-sql.3/20.3_exercise_bonus.sql
@@ -21,7 +21,8 @@ SELECT Scientist FROM Scientists.AssignedTo
 WHERE Project IN ('AeH3', 'Ast3', 'Che1');
 
 -- 5. Escreva uma query para exibir todas as informações de todos os projetos com mais de 500 horas.
-
+SELECT * FROM Scientists.Projects
+WHERE Hours > 500;
 
 -- 6. Escreva uma query para exibir todas as informações de todos os projetos cujas horas sejam maiores que 250 e menores 800.
 
