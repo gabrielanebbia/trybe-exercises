3a9b2aaeff1065e4267ec3b7f3b57c4eb18be615
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-08-23 17:31:34 -0300

    Exercicio 1.6

--- modulo_3.Back-end/bloco_20/introducao-a-sql.3/20.3_exercise_1.sql
+++ modulo_3.Back-end/bloco_20/introducao-a-sql.3/20.3_exercise_1.sql
@@ -27,6 +27,8 @@ WHERE code LIKE '%%F%';
 
 -- 6. Escreva uma query para exibir os fornecimentos onde as peças custam mais de R$15,00 e menos 
 -- de $40,00 . Ordene os resultados por ordem crescente.
-
+SELECT * FROM PecasFornecedores.Fornecimentos
+WHERE Preco BETWEEN 15 AND 40
+ORDER BY Preco ASC;
 
 -- 7. Escreva uma query para exibir o número de vendas feitas entre o dia 15/04/2018 e o dia 30/07/2019.
