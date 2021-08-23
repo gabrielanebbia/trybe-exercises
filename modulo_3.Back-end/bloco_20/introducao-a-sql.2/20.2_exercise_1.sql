33e4e56c2a415f2e86319084290506a62c75ee4e
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-08-23 16:32:28 -0300

    Exercicios 1 a 4

--- modulo_3.Back-end/bloco_20/introducao-a-sql.2/20.2_exercise_1.sql
+++ modulo_3.Back-end/bloco_20/introducao-a-sql.2/20.2_exercise_1.sql
@@ -1,9 +1,11 @@
 -- 1.Escreva uma query para exibir a string "This is SQL Exercise, Practice and Solution".
- 
+SELECT 'This is SQL Exercise, Practice and Solution';
+-- 2.Escreva uma query para exibir três números em três colunas.
+SELECT 1 AS 'Primeiro', 2 AS 'Segundo', 3 AS 'Terceiro';
 -- 3.Escreva uma query para exibir a soma dos números 10 e 15.
-
+SELECT 10 + 15;
 -- 4.Escreva uma query para exibir o resultado de uma expressão aritmética qualquer.
-
+SELECT 2 + 2 - 2 * 2 / 2;
 -- 5.Escreva uma query para exibir todas as informações de todos os cientistas.
 
 -- 6.Escreva uma query para exibir o nome como "Nome do Projeto" e as horas como "Tempo de Trabalho" de cada projeto.
