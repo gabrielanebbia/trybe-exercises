1aac56f9ffaab8ef9757416fdbcf9867db6a07dc
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-08-23 16:40:57 -0300

    Parte 2 - Exercicio 1

--- modulo_3.Back-end/bloco_20/introducao-a-sql.2/20.2_exercise_2.sql
+++ modulo_3.Back-end/bloco_20/introducao-a-sql.2/20.2_exercise_2.sql
@@ -1,4 +1,6 @@
 -- 1. Escreva uma query para exibir a peça e o preço de tudo que é provido pela empresa RBT.
+SELECT Piece, Price FROM Provides 
+WHERE Provider = 'RBT';
 
 -- 2. Escreva uma query para exibir todas as informações das cinco peças com os maiores preços.
 
