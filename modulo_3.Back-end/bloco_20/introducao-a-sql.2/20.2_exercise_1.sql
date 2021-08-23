f402f1e4a57428866d4c2285b81b21d66f6406da
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-08-23 16:35:07 -0300

    Exercicios 13 a 15

--- modulo_3.Back-end/bloco_20/introducao-a-sql.2/20.2_exercise_1.sql
+++ modulo_3.Back-end/bloco_20/introducao-a-sql.2/20.2_exercise_1.sql
@@ -23,7 +23,8 @@ SELECT DISTINCT Project FROM Scientists.AssignedTo;
 -- 12.Escreva uma query para exibir o nome do projeto com maior quantidade de horas.
 SELECT Name FROM Scientists.Projects ORDER BY Hours DESC LIMIT 1;
 -- 13.Escreva uma query para exibir o nome do segundo projeto com menor quantidade de horas.
-
+SELECT Name FROM Scientists.Projects ORDER BY Hours ASC LIMIT 1 OFFSET 1;
 -- 14.Escreva uma query para exibir todas as informações dos cinco projetos com a menor quantidade de horas.
-
+SELECT * FROM Scientists.Projects ORDER BY Hours ASC LIMIT 5;
 -- 15.Escreva uma query que exiba a string "Existem Number cientistas na tabela Scientists.", em que Number se refira a quantidade de cientistas.
+SELECT CONCAT('Existem ', COUNT(Name), ' cientistas na tabela Scientists.') AS 'Quantidade de Cientistas' FROM Scientists.Scientists; 
