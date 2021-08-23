19686f74d090205b7c0c7866fda5d00313932f8b
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-08-23 17:36:24 -0300

    Exercicio bonus 3

--- modulo_3.Back-end/bloco_20/introducao-a-sql.3/20.3_exercise_bonus.sql
+++ modulo_3.Back-end/bloco_20/introducao-a-sql.3/20.3_exercise_bonus.sql
@@ -12,7 +12,9 @@ ORDER BY Name ASC;
 
 -- 3. Escreva uma query para exibir o código e nome de todos os projetos que possuam em seu código o número 3. 
 -- Ordene o resultado em ordem alfabética.
-
+SELECT Code, Name FROM Scientists.Projects
+WHERE Code LIKE '%3%'
+ORDER BY Name;
 
 -- 4. Escreva uma query para exibir todos os cientistas(valores numéricos) cujos projetos sejam AeH3, Ast3 ou Che1.
 
