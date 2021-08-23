cb6285330d5d869abd91a03537f7937f08da5210
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-08-23 16:34:32 -0300

    Exercicios 9 a 12

--- modulo_3.Back-end/bloco_20/introducao-a-sql.2/20.2_exercise_1.sql
+++ modulo_3.Back-end/bloco_20/introducao-a-sql.2/20.2_exercise_1.sql
@@ -15,13 +15,13 @@ SELECT name FROM Scientists.Scientists ORDER BY name ASC;
 -- 8.Escreva uma query para exibir o nome dos Projetos em ordem alfabética descendente.
 SELECT Name FROM Scientists.Projects ORDER BY Name DESC;
 -- 9.Escreva uma query que exiba a string "O projeto Name precisou de Hours horas para ser concluído." para cada projeto.
-
+SELECT CONCAT('O projeto ', Name, ' precisou de ', Hours, ' horas para ser concluído.') AS 'Duração do Projeto'FROM Scientists.Projects;
 -- 10.Escreva uma query para exibir o nome e as horas dos três projetos com a maior quantidade de horas.
-
+SELECT Name, hours FROM Scientists.Projects ORDER BY Hours DESC LIMIT 3;
 -- 11.Escreva uma query para exibir o código de todos os projetos da tabela AssignedTo sem que haja repetições.
-
+SELECT DISTINCT Project FROM Scientists.AssignedTo;
 -- 12.Escreva uma query para exibir o nome do projeto com maior quantidade de horas.
-
+SELECT Name FROM Scientists.Projects ORDER BY Hours DESC LIMIT 1;
 -- 13.Escreva uma query para exibir o nome do segundo projeto com menor quantidade de horas.
 
 -- 14.Escreva uma query para exibir todas as informações dos cinco projetos com a menor quantidade de horas.
