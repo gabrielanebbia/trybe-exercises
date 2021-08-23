cee6a9f03f79cda98dbb6adeb46c02ae9ded99a5
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-08-23 17:31:16 -0300

    Exercicio 1.5

--- modulo_3.Back-end/bloco_20/introducao-a-sql.3/20.3_exercise_1.sql
+++ modulo_3.Back-end/bloco_20/introducao-a-sql.3/20.3_exercise_1.sql
@@ -22,7 +22,8 @@ WHERE name LIKE '%LTDA'
 ORDER BY name DESC;
 
 -- 5. Escreva uma query para exibir o número de empresas (fornecedores) que contém a letra F no código.
-
+SELECT COUNT(*) FROM PecasFornecedores.Fornecedores
+WHERE code LIKE '%%F%';
 
 -- 6. Escreva uma query para exibir os fornecimentos onde as peças custam mais de R$15,00 e menos 
 -- de $40,00 . Ordene os resultados por ordem crescente.
