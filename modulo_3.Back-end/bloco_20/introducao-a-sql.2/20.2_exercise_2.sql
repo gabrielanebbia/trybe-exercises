c7a9d225f53939055a450777a69165d80c7d7245
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-08-23 16:42:14 -0300

    Parte 2 - Exercicio 4

--- modulo_3.Back-end/bloco_20/introducao-a-sql.2/20.2_exercise_2.sql
+++ modulo_3.Back-end/bloco_20/introducao-a-sql.2/20.2_exercise_2.sql
@@ -16,5 +16,8 @@ OFFSET 3;
 
 -- 4. Escreva uma query para exibir todas as informações das peças que são providas pela empresa HAL. 
 -- Ordene o resultado pelos preços das peças de forma decrescente.
+SELECT * FROM Provides 
+WHERE Provider = 'HAL'
+ORDER BY Price DESC;
 
 -- 5. Escreva uma query para exibir por quantas empresas a peça 1 é provida.
