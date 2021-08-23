078e7a9b8881e875520d98f59200600c00a5b8c7
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-08-23 17:31:58 -0300

    Exercicio 1.7

--- modulo_3.Back-end/bloco_20/introducao-a-sql.3/20.3_exercise_1.sql
+++ modulo_3.Back-end/bloco_20/introducao-a-sql.3/20.3_exercise_1.sql
@@ -32,3 +32,5 @@ WHERE Preco BETWEEN 15 AND 40
 ORDER BY Preco ASC;
 
 -- 7. Escreva uma query para exibir o número de vendas feitas entre o dia 15/04/2018 e o dia 30/07/2019.
+SELECT COUNT(*) FROM PecasFornecedores.Vendas
+WHERE DATE(order_date) BETWEEN '2018-04-15' AND 2019-07-30;
