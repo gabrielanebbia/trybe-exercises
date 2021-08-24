a123c286944cd8ebfed792d90559bbe06951c0fd
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-08-24 20:20:17 -0300

    Exercicio 4

--- modulo_3.Back-end/bloco_21/funcoes-sql-joins-e-subqueries.1/21.1_exercise.sql
+++ modulo_3.Back-end/bloco_21/funcoes-sql-joins-e-subqueries.1/21.1_exercise.sql
@@ -14,7 +14,7 @@ ORDER BY average_salary DESC;
 
 -- 4. Escreva uma query que exiba a quantidade de dinheiro necessária para realizar o pagamento 
 -- de todas as pessoas funcionárias.
-
+SELECT SUM(SALARY) FROM hr.employees;
 
 -- 5. Escreva uma query que exiba quatro informações: o maior salário, o menor salário, a soma de todos os 
 -- salários e a média dos salários. Todos os valores devem ser formatados para ter apenas duas casas decimais.
