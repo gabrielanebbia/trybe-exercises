f55d6bbd1d517f6c97fdec975b026f1a117b6ef2
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-08-24 20:09:02 -0300

    Exercicio 2

--- modulo_3.Back-end/bloco_21/funcoes-sql-joins-e-subqueries.1/21.1_exercise.sql
+++ modulo_3.Back-end/bloco_21/funcoes-sql-joins-e-subqueries.1/21.1_exercise.sql
@@ -3,7 +3,7 @@
 SELECT MAX(SALARY) FROM hr.employees;
 
 -- 2. Escreva uma query que exiba a diferença entre o maior e o menor salário.
-
+SELECT MAX(SALARY) - MIN(SALARY) FROM hr.employees;
 
 -- 3. Escreva uma query que exiba a média salarial de cada JOB_ID, ordenando pela média salarial 
 -- em ordem decrescente.
