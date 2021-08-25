a2569cc2c5059da0d0e7997173741a46873b17d1
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-08-25 19:28:55 -0300

    Exercicio 2

--- modulo_3.Back-end/bloco_20/introducao-a-sql.4/20.4_exercise_1.sql
+++ modulo_3.Back-end/bloco_20/introducao-a-sql.4/20.4_exercise_1.sql
@@ -12,7 +12,9 @@
     
 -- Exercício 2: Procurando Nemo foi aclamado pela crítica! Foi classificado em 6.8, fez 450 milhões 
 -- no mercado interno e 370 milhões no mercado internacional. Adicione as informações à tabela BoxOffice.
-
+SELECT * FROM Pixar.BoxOffice;
+INSERT INTO Pixar.BoxOffice(movie_id, rating, domestic_sales, international_sales)
+	VALUE (9, 6.8, 450000000, 370000000);
 
 -- Exercício 3: O diretor do filme "Procurando Nemo" está incorreto, na verdade ele foi dirigido por 
 -- Andrew Staton. Corrija esse dado utilizando o UPDATE.
