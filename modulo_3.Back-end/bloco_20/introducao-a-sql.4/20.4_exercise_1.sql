b27945d4d5d384a3b163fec9653f48512e168ede
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-08-25 19:28:33 -0300

    Exercicio 1

--- modulo_3.Back-end/bloco_20/introducao-a-sql.4/20.4_exercise_1.sql
+++ modulo_3.Back-end/bloco_20/introducao-a-sql.4/20.4_exercise_1.sql
@@ -3,7 +3,12 @@
 	-- Procurando Nemo, de John Lasseter, lançado em 2003, com 107 minutos de duração.
 	-- Os Incríveis, de Brad Bird, lançado em 2004, com 116 minutos de duração.
 	-- WALL-E, de Pete Docter, lançada em 2008, com 104 minutos de duração.
-    
+   SELECT * FROM Pixar.Movies; 
+   INSERT INTO Pixar.Movies(title, director, year, length_minutes)
+	   VALUES ('Monstros SA', 'Pete Docter', 2001, 92),
+	   ('Procurando Nemo', 'John Lasseter', 2003, 107),
+	   ('Os Incríveis', 'Brad Bird', 2004, 116),
+	   ('WALL-E', 'Pete Docter', 2008, 104);
     
 -- Exercício 2: Procurando Nemo foi aclamado pela crítica! Foi classificado em 6.8, fez 450 milhões 
 -- no mercado interno e 370 milhões no mercado internacional. Adicione as informações à tabela BoxOffice.
