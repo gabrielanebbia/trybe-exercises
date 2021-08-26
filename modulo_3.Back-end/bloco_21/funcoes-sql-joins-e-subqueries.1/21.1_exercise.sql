f95529845340ad4845b645a0e1c8cadcc48b6778
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-08-26 00:59:36 -0300

    Exercicio 8

--- modulo_3.Back-end/bloco_21/funcoes-sql-joins-e-subqueries.1/21.1_exercise.sql
+++ modulo_3.Back-end/bloco_21/funcoes-sql-joins-e-subqueries.1/21.1_exercise.sql
@@ -37,7 +37,10 @@ GROUP BY JOB_ID;
 
 -- 8. Utilizando a query anterior, faça as alterações para que seja exibido somente a quantidade de 
 -- dinheiro necessária para cobrir a folha de pagamento das pessoas programadoras(IT_PROG).
-
+SELECT JOB_ID, SUM(SALARY) AS 'total_payments'
+FROM hr.employees
+GROUP BY JOB_ID
+HAVING JOB_ID = 'IT_PROG';
 
 -- 9. Escreva uma query que exiba em ordem decrescente a média salarial de todos os cargos, exceto 
 -- das pessoas programadoras ( IT_PROG ).
