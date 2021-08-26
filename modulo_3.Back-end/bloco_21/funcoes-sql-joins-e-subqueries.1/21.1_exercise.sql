4f250c429a3f69bfb341250b6b9b78d27a177190
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-08-26 01:26:20 -0300

    Exercicio 12

--- modulo_3.Back-end/bloco_21/funcoes-sql-joins-e-subqueries.1/21.1_exercise.sql
+++ modulo_3.Back-end/bloco_21/funcoes-sql-joins-e-subqueries.1/21.1_exercise.sql
@@ -67,7 +67,8 @@ SELECT * FROM hr.employees;
 
 -- 12. Escreva uma query que só exiba as informações dos funcionários cujo o primeiro nome tenha oito 
 -- ou mais caracteres.
-
+SELECT * FROM hr.employees
+WHERE LENGTH(FIRST_NAME) >= 8;
 
 -- 13. Escreva uma query que exiba as seguintes informações de cada funcionário: id, primeiro nome 
 -- e ano no qual foi contratado (exiba somente o ano).
