25fa17fc943e48a31dc046a191eeaa5641d118b2
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-08-26 01:34:08 -0300

    Exercicio 13 e 14

--- modulo_3.Back-end/bloco_21/funcoes-sql-joins-e-subqueries.1/21.1_exercise.sql
+++ modulo_3.Back-end/bloco_21/funcoes-sql-joins-e-subqueries.1/21.1_exercise.sql
@@ -73,12 +73,15 @@ WHERE LENGTH(FIRST_NAME) >= 8;
 -- e ano no qual foi contratado (exiba somente o ano).
 SELECT EMPLOYEE_ID, 
 FIRST_NAME, 
-YEAR(HIRE_DATE) 
+YEAR(HIRE_DATE) AS HIRE_DAY
 FROM hr.employees;
 
 -- 14. Escreva uma query que exiba as seguintes informações de cada funcionário: id, primeiro nome 
 -- e dia do mês no qual foi contratado (exiba somente o dia).
-
+SELECT EMPLOYEE_ID, 
+FIRST_NAME, 
+RIGHT(HIRE_DATE, 2) AS HIRE_DAY
+FROM hr.employees;
 
 -- 15. Escreva uma query que exiba as seguintes informações de cada funcionário: id, primeiro nome 
 -- e mês no qual foi contratado (exiba somente o mês).
