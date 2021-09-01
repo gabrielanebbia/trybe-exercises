5821f2928840ca4f8255f8387201de6584d03d95
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-09-01 00:18:09 -0300

    Exercicio 2

--- modulo_3.Back-end/bloco_21/funcoes-sql-joins-e-subqueries.2/21.2_exercise_bonus.sql
+++ modulo_3.Back-end/bloco_21/funcoes-sql-joins-e-subqueries.2/21.2_exercise_bonus.sql
@@ -9,8 +9,11 @@ ON m.id = b.movie_id
 WHERE b.rating > 8
 AND m.theater_id IS NOT NULL;
 
--- 11. Utilizando o SELF JOIN , selecione os títulos e duração dos filmes que possuem o mesmo diretor.
-
+-- 11. Utilizando o SELF JOIN, selecione os títulos e duração dos filmes que possuem o mesmo diretor.
+SELECT m1.title, m1.length_minutes, m2.title, m2.length_minutes
+FROM Pixar.Movies AS m1,
+Pixar.Movies AS m2
+WHERE m1.director = m2.director;
 
 -- 12. Faça duas buscas, uma utilizando SUBQUERY e outra utilizando INNER JOIN , que retornem o título
 -- dos filmes que arrecadaram 500 milhões ou mais, e que possuem duração maior que 110 minutos.
