53903a110dea8b552f06dfca4ceba51a249571e5
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-08-31 23:22:51 -0300

    Exercicio 4

--- modulo_3.Back-end/bloco_21/funcoes-sql-joins-e-subqueries.2/21.2_exercise_1.sql
+++ modulo_3.Back-end/bloco_21/funcoes-sql-joins-e-subqueries.2/21.2_exercise_1.sql
@@ -25,7 +25,17 @@ ORDER BY b.rating DESC;
 -- Exercício 4: Utilizando o LEFT JOIN, faça uma busca que retorne todos os dados dos cinemas, mesmo os que não 
 -- possuem filmes em cartaz e, adicionalmente, os dados dos filmes que estão em cartaz nestes cinemas. 
 -- Retorne os nomes dos cinemas em ordem alfabética.
-
+SELECT *
+FROM Pixar.Theater AS t
+LEFT JOIN  Pixar.Movies AS m
+ON t.id = m.theater_id
+ORDER BY t.name ASC;
+-- ou (sem incluir os id no resultado)
+SELECT t.name, t.location, m.title, m.director, m.length_minutes, m.year
+FROM Pixar.Theater AS t
+LEFT JOIN  Pixar.Movies AS m
+ON t.id = m.theater_id
+ORDER BY t.name ASC;
 
 -- Exercício 5: Utilizando o RIGHT JOIN, faça uma busca que retorne todos os dados dos filmes, mesmo os que não 
 -- estão em cartaz e, adicionalmente, os dados dos cinemas que possuem estes filmes em cartaz. Retorne os nomes 
