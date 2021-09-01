edd6733d55d42f79096aae879ff643ab7883a5c6
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-09-01 00:17:47 -0300

    Exercicio 1

--- modulo_3.Back-end/bloco_21/funcoes-sql-joins-e-subqueries.2/21.2_exercise_bonus.sql
+++ modulo_3.Back-end/bloco_21/funcoes-sql-joins-e-subqueries.2/21.2_exercise_bonus.sql
@@ -2,7 +2,12 @@
 
 -- 10. Utilizando o INNER JOIN, selecione todas as informações dos filmes com avaliação maior que 8 
 -- e que estejam em cartaz.
-
+SELECT *
+FROM Pixar.Movies AS m
+INNER JOIN  Pixar.BoxOffice AS b
+ON m.id = b.movie_id
+WHERE b.rating > 8
+AND m.theater_id IS NOT NULL;
 
 -- 11. Utilizando o SELF JOIN , selecione os títulos e duração dos filmes que possuem o mesmo diretor.
 
