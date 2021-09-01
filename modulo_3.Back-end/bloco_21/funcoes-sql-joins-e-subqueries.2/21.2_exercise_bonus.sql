a53a260e1f678da83d4df3b83aa0bb6559ccca50
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-08-31 23:50:50 -0300

    Adiciona arquivo do exercicio bonus

---
+++ modulo_3.Back-end/bloco_21/funcoes-sql-joins-e-subqueries.2/21.2_exercise_bonus.sql
@@ -0,0 +1,12 @@
+-- Exercício Bônus:
+
+-- 10. Utilizando o INNER JOIN, selecione todas as informações dos filmes com avaliação maior que 8 
+-- e que estejam em cartaz.
+
+
+-- 11. Utilizando o SELF JOIN , selecione os títulos e duração dos filmes que possuem o mesmo diretor.
+
+
+-- 12. Faça duas buscas, uma utilizando SUBQUERY e outra utilizando INNER JOIN , que retornem o título
+-- dos filmes que arrecadaram 500 milhões ou mais, e que possuem duração maior que 110 minutos.
+
