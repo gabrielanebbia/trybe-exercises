9962e00fb2f905327e418fc2e678a7fb1843017e
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-09-01 00:18:41 -0300

    Exercicio 3

--- modulo_3.Back-end/bloco_21/funcoes-sql-joins-e-subqueries.2/21.2_exercise_bonus.sql
+++ modulo_3.Back-end/bloco_21/funcoes-sql-joins-e-subqueries.2/21.2_exercise_bonus.sql
@@ -18,3 +18,19 @@ WHERE m1.director = m2.director;
 -- 12. Faça duas buscas, uma utilizando SUBQUERY e outra utilizando INNER JOIN , que retornem o título
 -- dos filmes que arrecadaram 500 milhões ou mais, e que possuem duração maior que 110 minutos.
 
+-- Usando SUBQUERY
+SELECT title FROM Pixar.Movies
+WHERE id IN (
+	SELECT movie_id 
+    FROM Pixar.BoxOffice
+    WHERE international_sales >= 500000000
+)
+AND length_minutes > 110;
+    
+-- Usando INNER JOIN
+SELECT m.title
+FROM Pixar.Movies AS m
+INNER JOIN  Pixar.BoxOffice AS b
+ON m.id = b.movie_id
+WHERE b.international_sales >= 500000000
+AND m.length_minutes > 110;
