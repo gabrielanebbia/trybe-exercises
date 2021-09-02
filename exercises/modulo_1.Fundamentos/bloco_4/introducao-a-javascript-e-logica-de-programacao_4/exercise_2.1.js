1195cdd249e7d6b31b043609e7d3243e881ed92f
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-09-01 23:11:39 -0300

    Organiza diretorios do bloco 5

--- exercises/modulo_1.Fundamentos/bloco_4/introducao-a-javascript-e-logica-de-programacao_4/exercise_2.1.js
+++
@@ -1,5 +0,0 @@
-//Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
-
-function(verificaPalindrome) {
-    if (verificaPalindrome)
-}
--- exercises/modulo_1.Fundamentos/bloco_5/javascript-dom-eventos-e-web-storage_2/README.md
+++
@@ -1,4 +0,0 @@
-5.2 - JavaScript - Trabalhando com elementos
-
-    Part 1 - Criando elementos;
-    Part 2 - Removendo elementos;
