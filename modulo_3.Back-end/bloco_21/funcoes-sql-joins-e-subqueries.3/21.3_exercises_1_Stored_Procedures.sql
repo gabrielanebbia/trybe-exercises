6fa1db4e70dcfa65b9f29bcf160084b427f1543b
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-09-13 18:25:55 -0300

    Exercicio 2 - STORED PROCEDURES

--- modulo_3.Back-end/bloco_21/funcoes-sql-joins-e-subqueries.3/21.3_exercises_1_Stored_Procedures.sql
+++ modulo_3.Back-end/bloco_21/funcoes-sql-joins-e-subqueries.3/21.3_exercises_1_Stored_Procedures.sql
@@ -21,8 +21,27 @@ DELIMITER ;
 CALL top_10_actors();
 
 -- 2.Monte uma procedure que receba como parâmetro de entrada o nome da categoria desejada em uma string 
--- e que exiba o id do filme , seu titulo, o id de sua categoria e o nome da categoria selecionada. 
+-- e que exiba o id do filme, seu titulo, o id de sua categoria e o nome da categoria selecionada. 
 -- Use as tabelas film, film_category e category para montar essa procedure.
+
+USE sakila;
+DELIMITER $$
+
+CREATE PROCEDURE movie_by_category(IN category VARCHAR(25))
+BEGIN
+	SELECT f.film_id, f.title, fc.category_id, c.name
+    FROM sakila.film AS f
+    INNER JOIN sakila.film_category AS fc 
+    ON f.film_id = fc.film_id
+    INNER JOIN sakila.category AS c 
+    ON fc.category_id = c.category_id
+    WHERE c.name = category;
+END $$
+
+DELIMITER ;
+
+-- Testando:
+CALL movie_by_category('Games');
 
 -- 3.Monte uma procedure que receba o email de um cliente como parâmetro de entrada e diga se o cliente 
 -- está ou não ativo, através de um parâmetro de saída.
