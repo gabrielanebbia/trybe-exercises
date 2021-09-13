f10c888dab383efeb217b12640628c0a6a910a03
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-09-13 20:42:15 -0300

    Refatora exercicio parte 1 - bonus

--- modulo_1.Fundamentos/bloco_07/introducao-a-javascript-es6-e-testes-unitarios_1/parte1_exercise_bonus.js
+++ modulo_1.Fundamentos/bloco_07/introducao-a-javascript-es6-e-testes-unitarios_1/parte1_exercise_bonus.js
@@ -2,8 +2,12 @@
 
 const oddsAndEvens = [13, 3, 4, 10, 7, 2]
 
-oddsAndEvens.sort((param1, param2) => param1 - param2);
+const arrayOfNumbers = array => {
+  array.sort((param1, param2) => param1 - param2);
+  return array;
+}
 
-console.log(oddsAndEvens);
+const sortedArray = arrayOfNumbers(oddsAndEvens);
+console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente!`);
 
 // Referencia: https://ricardo-reis.medium.com/o-m%C3%A9todo-sort-do-array-javascript-482576734e0a#:~:text=Por%20padr%C3%A3o%2C%20o%20m%C3%A9todo%20sort,para%20determinar%20as%20suas%20ordens.
