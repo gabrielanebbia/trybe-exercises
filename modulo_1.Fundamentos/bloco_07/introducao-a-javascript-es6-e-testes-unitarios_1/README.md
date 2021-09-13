9739cb3e6157449f1267e19657d9a22d1aedfff8
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-09-13 20:35:53 -0300

    Refatora exercicio 1.1

--- modulo_1.Fundamentos/bloco_07/introducao-a-javascript-es6-e-testes-unitarios_1/README.md
+++ modulo_1.Fundamentos/bloco_07/introducao-a-javascript-es6-e-testes-unitarios_1/README.md
@@ -3,7 +3,7 @@
 
 #### Parte I
 
-1 - Fazendo as modificações necessárias na função:
+1 - Faz as modificações necessárias na função dada:
 - [x] Faz com que seu comportamento respeite o escopo no qual cada variável foi declarada
 - [x] Modica a estrutura da função para que ela seja uma arrow function
 - [x] Modica as concatenações para template literals
--- modulo_1.Fundamentos/bloco_07/introducao-a-javascript-es6-e-testes-unitarios_1/parte1_exercise_1.js
+++ modulo_1.Fundamentos/bloco_07/introducao-a-javascript-es6-e-testes-unitarios_1/parte1_exercise_1.js
@@ -17,15 +17,20 @@ testingScope(true);
 */
 
 // Resultado
-const testingScope = escopo => { 
-  if (escopo === true) { 
-    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-    console.log(ifScope);
-  } else {
-    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-    console.log(elseScope);
+const oddsAndEvens = [13, 3, 4, 10, 7, 2]
+
+const arrayOfNumbers = (array) => {
+  array.sort(function comparaValores(param1, param2) {
+  if (param1 > param2) {
+    return 1;
+  } else if (param1 < param2) {
+  return -1;
   }
+  return 0;
+});
+return array;
 }
 
-testingScope(true);
+const sortedArray = arrayOfNumbers(oddsAndEvens); 
+
+console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente!`);
