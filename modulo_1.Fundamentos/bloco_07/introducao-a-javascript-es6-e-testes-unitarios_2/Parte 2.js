61678c9c0ed9b180deca069d46b31e40781e63ea
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-09-14 17:34:28 -0300

    Exercicio 2.6 - Funcao que retorna o total de estudantes

--- modulo_1.Fundamentos/bloco_07/introducao-a-javascript-es6-e-testes-unitarios_2/Parte 2.js
+++ modulo_1.Fundamentos/bloco_07/introducao-a-javascript-es6-e-testes-unitarios_2/Parte 2.js
@@ -43,3 +43,15 @@ listValues(lesson3);
 
 const allLessons = Object.assign({lesson1, lesson2, lesson3});
 console.log(allLessons);
+
+const numberOfStudents = (obj) => {
+  const array = Object.keys(obj);
+
+  let total = 0;
+  for (index in array) {
+    total += obj[array[index]].numeroEstudantes;
+  }
+
+  return total;
+};
+console.log(numberOfStudents(allLessons));
