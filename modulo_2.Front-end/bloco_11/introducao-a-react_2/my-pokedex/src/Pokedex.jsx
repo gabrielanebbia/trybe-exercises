5246c7ca49a76d097eefc150709bb784919ef9c5
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-09-23 19:32:36 -0300

    Organiza diretorio e readme

---
+++ modulo_2.Front-end/bloco_11/introducao-a-react_2/my-pokedex/src/Pokedex.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Data from './data';
+import Pokemon from './Pokemon';
+
+class Pokedex extends React.Component {
+  render() {
+
+    return (
+      <div className='pokedex'>
+       { Data.map(item => 
+       <Pokemon 
+          name={item.name} 
+          type={item.type} 
+          weight={item.averageWeight.value} 
+          unit={item.averageWeight.measurementUnit} 
+          image={item.image}
+       />)}
+      </div>
+    );
+  }
+}
+
+export default Pokedex;
