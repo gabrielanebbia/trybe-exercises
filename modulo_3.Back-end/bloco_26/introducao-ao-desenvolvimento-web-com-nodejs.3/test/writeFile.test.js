58fa4d5a3b5de0b66e85dda2c51a9d92316567e5
Gabriela Nebbia <gabrielanebbia@gmail.com>
2022-01-19 01:32:04 -0300

    test: adapta teste adicionando stub ao modulo fs

--- modulo_3.Back-end/bloco_26/introducao-ao-desenvolvimento-web-com-nodejs.3/test/writeFile.test.js
+++ modulo_3.Back-end/bloco_26/introducao-ao-desenvolvimento-web-com-nodejs.3/test/writeFile.test.js
@@ -1,7 +1,18 @@
 const { expect } = require('chai');
+const fs = require('fs');
+const sinon = require('sinon');
+
 const writeFile = require('../writeFile');
 
 describe('Executa a função writeFile', () => {
+  before(() => {
+    sinon.stub(fs, 'writeFileSync');
+  });
+
+  after(() => {
+    fs.writeFileSync.restore();
+  });
+
   describe('a resposta', () => {
     it('é uma "string"', () => {
       const resposta = writeFile('file.txt', 'conteúdo do arquivo');
