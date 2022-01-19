f64e1e071fbfa8fa4a57d9a5c6bc2729d3ec82ed
Gabriela Nebbia <gabrielanebbia@gmail.com>
2022-01-19 00:16:12 -0300

    feat: implementa na funcao a validacao para passar no teste

--- modulo_3.Back-end/bloco_26/introducao-ao-desenvolvimento-web-com-nodejs.3/verifyNumber.js
+++ modulo_3.Back-end/bloco_26/introducao-ao-desenvolvimento-web-com-nodejs.3/verifyNumber.js
@@ -1,4 +1,8 @@
 function verifyNumber(number) {
+  if (typeof number !== 'number') {
+    return 'o valor deve ser um número';
+  }
+
   if (number > 0) return 'positivo';
 
   if (number < 0) return 'negativo';
