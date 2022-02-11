7a8461d5c0ddc1734609c1017ee8f30ca1e4cadf
Gabriela Nebbia <gabrielanebbia@gmail.com>
2022-02-11 01:16:50 -0300

    fix: remove arquivo temporario

--- modulo_4.Ciencia_da_Computacao/introducao-a-python.1/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-def get_biggest_word(name_list):
-    biggest_word = ''
-    for name in name_list:
-        if len(name) > len(biggest_word):
-            biggest_word = name
-    return biggest_word
-
-print(get_biggest_word(["nome", "lista", "palavra"]))
