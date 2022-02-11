3c6a504247767ccbbab76fd0197b28765958e270
Gabriela Nebbia <gabrielanebbia@gmail.com>
2022-02-10 23:53:02 -0300

    feat(exercicio 2): adiciona funcao para calculo da media

--- modulo_4.Ciencia_da_Computacao/introducao-a-python.1/exercicios.py
+++ modulo_4.Ciencia_da_Computacao/introducao-a-python.1/exercicios.py
@@ -5,3 +5,13 @@ def higher_number(x, y):
     if x > y:
         return x
     return y
+
+
+""" Exercício 2 """
+
+
+def average(list):
+    sum = 0
+    for number in list:
+        sum += number
+    return sum / len(list)
