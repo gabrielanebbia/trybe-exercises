0c250ae7e383e07cc506ccae991ad5d9f1ba3e7d
Gabriela Nebbia <gabrielanebbia@gmail.com>
2022-03-22 22:09:43 -0300

    feat(bonus 4): adiciona funcao que calcula preco do combustivel

--- modulo_4.Ciencia_da_Computacao/introducao-a-python.1/exercicios_bonus.py
+++ modulo_4.Ciencia_da_Computacao/introducao-a-python.1/exercicios_bonus.py
@@ -25,3 +25,27 @@ def sum_numbers(N):
     for number in range(1, N + 1):
         sum += number
     return sum
+
+
+""" Exercício bônus 4 """
+
+
+def fuel_price(amount_liters, fuel_type):
+    if fuel_type == 'A':
+        price = 1.90
+        if amount_liters < 20:
+            discount = 0.03
+        else:
+            discount = 0.05
+
+    if fuel_type == 'G':
+        price = 2.50
+        if amount_liters < 20:
+            discount = 0.04
+        else:
+            discount = 0.06
+
+    total_for_liters = price * amount_liters
+    total_discount = total_for_liters * discount
+    total = total_for_liters - total_discount
+    return total
