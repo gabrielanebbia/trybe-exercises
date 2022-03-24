0f6c2eafc005f55c7274e09c34882f9f11295dab
Gabriela Nebbia <gabrielanebbia@gmail.com>
2022-03-24 02:09:47 -0300

    fix: altera nomes das variaveis, atributos, classes e ids

--- modulo_1.Fundamentos/bloco_06/html-e-css-forms-flexbox-e-responsivo_1/script.js
+++ modulo_1.Fundamentos/bloco_06/html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -1,55 +1,54 @@
 // Cria através de uma estrutura de repetição os <option>
-let estados = ['Selecione','Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal',  'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
+let states = ['Selecione','Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal',  'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
 
-let secaoEstado = document.querySelector('#userEstado');
+let selectState = document.querySelector('#userState');
 
-for (let index = 0; index < estados.length; index += 1) {
-  let novoEstado = document.createElement('option');
-  novoEstado.innerText = estados[index];
-  secaoEstado.appendChild(novoEstado);
+for (let index = 0; index < states.length; index += 1) {
+  let newState = document.createElement('option');
+  newState.innerText = states[index];
+  selectState.appendChild(newState);
 }
 
 //Cria alertas de erro para datas inválidas
-function testDataInicial() {
-let dataInput = document.getElementById('userDataInicio').value;
+function testInitialDate() {
+let dateInput = document.getElementById('userInitialDate').value;
 
-  for (let index = 0; index < dataInput.length; index += 1) {
-    if (dataInput[index] === '-') {
+  for (let index = 0; index < dateInput.length; index += 1) {
+    if (dateInput[index] === '-') {
     alert('Data inválida');
     }
-    if ((dataInput[0] + dataInput[1]) > 31) {
+    if ((dateInput[0] + dateInput[1]) > 31) {
       alert('Data inválida');
     }
-    if ((dataInput[2] + dataInput[3]) > 12) {
+    if ((dateInput[2] + dateInput[3]) > 12) {
       alert('Mês inválido');
     }
   }
 }
 
-testDataInicial();
+testInitialDate();
 
-dataInput.addEventListener('keyup', testDataInicial);
+dateInput.addEventListener('keyup', testInitialDate);
 
 // Botão para criar uma div com o conteúdo inserido no formulário
 // Referência: https://developer.mozilla.org/pt-BR/docs/Web/API/EventTarget/addEventListener e https://developer.mozilla.org/pt-BR/docs/Web/API/Event/preventDefault
 
 let forms = document.getElementById('forms');
-let criar = document.getElementById('buttonCriar');
+let buttonCreateResume = document.getElementById('buttonCreateResume');
 
 forms.addEventListener('submit', function (event) {
   event.preventDefault();
-  let valid = false;
+  let isValid = false;
 
-  if (valid = true) {
-    criar.addEventListener('click', createCurriculo())
+  if (isValid = true) {
+    buttonCreateResume.addEventListener('click', createResume())
   }
 });
 
-function createCurriculo() {
-  let curriculo = document.querySelector('#curriculo');
-  let part1 = document.createElement('div');
-  curriculo.appendChild(part1);
-
+function createResume() {
+  let resume = document.querySelector('#resume');
+  let section1 = document.createElement('div');
+  resume.appendChild(section1);
 }
 
-criar.addEventListener('click', createCurriculo);
+buttonCreateResume.addEventListener('click', createResume);
