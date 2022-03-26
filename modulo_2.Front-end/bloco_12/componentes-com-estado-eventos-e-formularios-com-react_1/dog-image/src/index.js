63812868120ada772f01ca4db51c338dab19e807
Gabriela Nebbia <gabrielanebbia@gmail.com>
2022-03-26 00:41:55 -0300

    chore: cria projeto react

---
+++ modulo_2.Front-end/bloco_12/componentes-com-estado-eventos-e-formularios-com-react_1/dog-image/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>Dog Image</div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
