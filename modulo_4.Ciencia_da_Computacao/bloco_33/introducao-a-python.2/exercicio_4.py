1ef1f0352730894e45850a0dffda592c61590380
Gabriela Nebbia <gabrielanebbia@gmail.com>
2022-04-29 22:07:03 -0300

    feat(exercicio 4): adiciona funcao para contar livros por categoria

--- modulo_4.Ciencia_da_Computacao/bloco_33/introducao-a-python.2/exercicio_4.py
+++ modulo_4.Ciencia_da_Computacao/bloco_33/introducao-a-python.2/exercicio_4.py
@@ -6,5 +6,18 @@ def get_books_in_json_file(file_path):
         return [json.loads(book) for book in file]
 
 
+def count_books_by_categories(books):
+    categories = {}
+
+    for book in books:
+        for category in book["categories"]:
+            if not categories.get(category):
+                categories[category] = 0
+            categories[category] += 1
+    return categories
+
+
 if __name__ == "__main__":
     books = get_books_in_json_file("exercicio_4.json")
+    books_by_categories = count_books_by_categories(books)
+    print(books_by_categories)
